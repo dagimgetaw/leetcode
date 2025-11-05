e0026855117e62d700c473f93aea68264161b704
Dagim <dagimgetaW27@gmail.com>
2025-11-05 21:26:05 +0300

    feat: solve 20. Valid Parentheses

---
+++ easy/stack/valid_parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+            stack = []
+
+            for char in s:
+                if char == "(" or char == "[" or char == "{":
+                    stack.append(char)
+                    
+                elif char == ")" or char == "]" or char == "}":
+                    if not stack: return False
+
+                    top = stack[-1]
+                    
+                    if ((char == ")" and top != "(") or 
+                    (char == "]" and top != "[") or 
+                    (char =="}" and top != "{")): return False
+
+                    stack.pop()
+                
+            return not stack
