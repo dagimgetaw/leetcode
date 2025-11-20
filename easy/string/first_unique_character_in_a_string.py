ea62a98c2db904cb21f6acdb48ea32c21a45544d
Dagim <dagimgetaW27@gmail.com>
2025-11-20 18:06:36 +0300

    feat: solve 387. First Unique Character in a String

---
+++ easy/string/first_unique_character_in_a_string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        freq = {}
+
+        for c in s:
+            freq[c] = freq.get(c, 0) + 1
+
+        for i, c in enumerate(s):
+            if freq[c] == 1:
+                return i
+
+        return -1
+    
