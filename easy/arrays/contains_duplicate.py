a2a4bb5729f781ee8bc8fc14ada03091481470f5
Dagim <dagimgetaW27@gmail.com>
2025-12-08 14:47:56 +0300

    feat: solve 217. Contains Duplicate

---
+++ easy/arrays/contains_duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash = set()
+        
+        for n in nums:
+            if n in hash: return True
+            else: hash.add(n)
+            
+        return False
+        
