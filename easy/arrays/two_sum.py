87018045bfa3cb652cc927b06ae0133177df17ba
Dagim <dagimgetaW27@gmail.com>
2025-12-09 22:42:32 +0300

    feat: solve 1. Two Sum

--- easy/arrays/two_sum.py
+++ easy/arrays/two_sum.py
@@ -1,9 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        output = []
-        for i in range(0, len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if (nums[i] + nums[j] == target):
-                    output.append(i)
-                    output.append(j)
-                    return output
+        hashmap = {}
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            
+            if diff in hashmap: return (i, hashmap[diff])
+            hashmap[n] = i
+        
+        return []
+        
