5f12927a536b1a0685c20e0e5b3a14a5cf4e4f9a
Dagim <dagimgetaW27@gmail.com>
2026-01-19 14:35:02 +0300

    feat: solve 1189. Maximum Number of Balloons

--- easy/hashmap/maximum_number_of_balloons.py
+++ easy/hashmap/maximum_number_of_balloons.py
@@ -1,16 +1,11 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         from collections import Counter
-        
+
         counter = Counter(text)
-        balloon = Counter("ballon")
+        balloon = Counter("balloon")
         res = len(text)
         for c in balloon:
             res = min(res, counter[c] // balloon[c])
             
         return res
-        
-    
-text = "nlaebolko"
-sol = Solution()
-print(sol.maxNumberOfBalloons(text))
