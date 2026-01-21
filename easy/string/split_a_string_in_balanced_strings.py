42759551ff9c38edc8f423f641164d28a5fe959f
Dagim <dagimgetaW27@gmail.com>
2026-01-22 01:05:04 +0300

    feat: solve 1221. Split a String in Balanced Strings

---
+++ easy/string/split_a_string_in_balanced_strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        count = 0
+        res = 0
+
+        for c in s:
+            if c == "R":
+                count += 1
+            else:
+                count -= 1
+                
+            if count == 0:
+                res += 1
+                
+        return res
+        
