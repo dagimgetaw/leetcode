33d90088cdd8fd05305d82f65ae0d1c82bb38767
Dagim <dagimgetaW27@gmail.com>
2026-03-30 16:04:22 +0300

    feat: solve 2194. Cells in a Range on an Excel Sheet

---
+++ easy/arrays/cells_in_a_range_on_an_excel_sheet.py
@@ -0,0 +1,18 @@
+class Solution:
+    def cellsInRange(self, s: str) -> List[str]:
+        start, end = s.split(":")
+        
+        start_col = start[0]
+        end_col = end[0]
+        start_row = int(start[1:])
+        end_row = int(end[1:])
+        
+        arr = []
+        
+        for col in range(ord(start_col), ord(end_col) + 1):
+            for row in range(start_row, end_row + 1):
+                arr.append(chr(col) + str(row))
+                
+        return arr
+
+            
