bc5fb9507ce257888b5000c802ffc8c6d5f5a591
Dagim <dagimgetaW27@gmail.com>
2026-04-30 11:24:08 +0300

    feat: solve 1282. Group the People Given the Group Size They Belong To

---
+++ medium/array/group_the_people_given_the_group_size_they_belong_to.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        from collections import defaultdict
+        
+        groups = defaultdict(list)
+        result = []
+
+        for i, size in enumerate(groupSizes):
+            groups[size].append(i)
+
+            if len(groups[size]) == size:
+                result.append(groups[size])
+                groups[size] = []
+
+        return result
+        
